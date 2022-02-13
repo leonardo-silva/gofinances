20965846993560f9e61e2f5396988fe338e6e695
Leonardo Silva <leonardosilvaif@hotmail.com>
2022-02-13 10:32:47 -0300

    Implementing styled-components for UserInfo inside the header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 
-import { Container, Header } from "./styles";
+import { 
+    Container, 
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+ } from "./styles";
 
 export function Dashboard() {
     return (
         <Container>
             <Header>
+                <UserInfo>
+                    <Photo source={{ uri: 'https://github.com/leonardo-silva.png'}}/>
+                    <User>
+                        <UserGreeting>Hi, </UserGreeting>
+                        <UserName>Leonardo</UserName>
+                    </User>
+                </UserInfo>
                 
             </Header>
         </Container>
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import { RFPercentage } from "react-native-responsive-fontsize";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
     flex: 1;
@@ -10,4 +10,40 @@ export const Header = styled.View`
     width: 100%;
     height: ${RFPercentage(40)}px;
     background-color: ${({ theme }) => theme.colors.primary};
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const UserInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+    // Instructor put properties below in a UserWrapper (parent of UserInfo);
+    width: 100%;
+        // the padding below has the effect of padding-horizontal only, up-down (vertical) is zero;
+    padding: 0 24px;
+`;
+
+export const Photo = styled.Image`
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+    border-radius: 10px;
+`;
+
+export const User = styled.View`
+    margin-left: ${RFValue(18)}px;
+`;
+
+export const UserGreeting = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+    font-size: ${RFValue(18)}px;
+    line-height: ${RFValue(24)}px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+    font-size: ${RFValue(18)}px;
+    line-height: ${RFValue(24)}px;
+    font-family: ${({ theme }) => theme.fonts.bold};
 `;
