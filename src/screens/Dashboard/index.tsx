5a7b11307d8c3095a39fc2bee64067c28d105a49
Leonardo Silva <leonardosilvaif@hotmail.com>
2022-02-19 14:46:49 -0300

    Using vector-icons with styled-components inside the Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -7,21 +7,27 @@ import {
     Photo,
     User,
     UserGreeting,
-    UserName
+    UserName,
+    Icon,
+    UserWrapper
  } from "./styles";
 
 export function Dashboard() {
     return (
         <Container>
             <Header>
-                <UserInfo>
-                    <Photo source={{ uri: 'https://github.com/leonardo-silva.png'}}/>
-                    <User>
-                        <UserGreeting>Hi, </UserGreeting>
-                        <UserName>Leonardo</UserName>
-                    </User>
-                </UserInfo>
-                
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://github.com/leonardo-silva.png'}}/>
+                        <User>
+                            <UserGreeting>Hi, </UserGreeting>
+                            <UserName>Leonardo</UserName>
+                        </User>
+                    </UserInfo>
+
+                    <Icon name="power"/>
+
+                </UserWrapper>    
             </Header>
         </Container>
     )
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { Feather } from '@expo/vector-icons';
 
 export const Container = styled.View`
     flex: 1;
@@ -15,13 +16,18 @@ export const Header = styled.View`
     justify-content: center;
 `;
 
+export const UserWrapper = styled.View`
+    width: 100%;
+    // the padding below has the effect of padding-horizontal only, up-down (vertical) is zero;
+    padding: 0 24px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
 export const UserInfo = styled.View`
     flex-direction: row;
     align-items: center;
-    // Instructor put properties below in a UserWrapper (parent of UserInfo);
-    width: 100%;
-        // the padding below has the effect of padding-horizontal only, up-down (vertical) is zero;
-    padding: 0 24px;
 `;
 
 export const Photo = styled.Image`
@@ -46,4 +52,9 @@ export const UserName = styled.Text`
     font-size: ${RFValue(18)}px;
     line-height: ${RFValue(24)}px;
     font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const Icon = styled(Feather)`
+    color: ${({ theme }) => theme.colors.secondary};
+    font-size: ${RFValue(24)}px;
 `;
