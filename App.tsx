ad812659ca7bdfe9f125c085a0125d82a2588d7f
Leonardo Silva <leonardosilvaif@hotmail.com>
2022-04-19 13:06:53 -0300

    Setting up Tab Bar Navigator (react-navigation)

--- App.tsx
+++ App.tsx
@@ -9,9 +9,11 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 
-import theme from './src/global/styles/theme';
+import { NavigationContainer } from '@react-navigation/native';
 
-import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
+
+import theme from './src/global/styles/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +28,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
